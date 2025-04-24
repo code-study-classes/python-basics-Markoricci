d86c4faf4ed21cb4747f40d0cd02acc77ed7afa5
Mark <146416345+Markoricci@users.noreply.github.com>
2025-04-25 02:03:21 +0300

    Update numbers.py

--- practice_package/numbers.py
+++ practice_package/numbers.py
@@ -1,18 +1,18 @@
-def calculate_distance(x, y):
-    pass
+def calculate_distance(x1, x2):
+    return abs(x1 - x2)
 
+def calculate_segments(length_a, length_b):
+    if length_a <= length_b:
+        raise ValueError
+    return length_a // length_b
 
-def calculate_segments(a, b):
-    pass
+def calculate_digit_sum(number):
+    return sum(int(digit) for digit in str(abs(number)))
 
+def calculate_rect_area(x1, y1, x2, y2):
+    width = abs(x2 - x1)
+    height = abs(y2 - y1)
+    return width * height
 
-def calculate_digit_sum():
-    pass
-
-
-def round_to_multiple():
-    pass
-
-
-def calculate_rect_area():
-    pass
+def round_to_multiple(number, multiple):
+    return round(number / multiple) * multiple
