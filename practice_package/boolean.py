efff6069b0e6fe303d4586909fd4c853e2a9d762
Mark <146416345+Markoricci@users.noreply.github.com>
2025-04-25 09:56:11 +0300

    Update boolean.py

---
+++ practice_package/boolean.py
@@ -0,0 +1,20 @@
+def check_between_numbers(a, b, c):
+    return (a < b < c) or (c < b < a)
+
+def check_odd_three(n):
+    return 99 < abs(n) < 1000 and abs(n) % 2 == 1
+
+def check_unique_digits(n):
+    return (
+    99 < abs(n) < 1000 and len(set(str(abs(n)))) == 3
+)
+
+def check_palindrome_number(n):
+    return str(abs(n)) == str(abs(n))[::-1]
+
+def check_ascending_digits(n):
+    return (
+    all(map(lambda x: x[0] < x[1], 
+            zip(map(int, str(abs(n))), map(int, str(abs(n))[1:]))))
+    if 99 < abs(n) < 1000 else False
+)
